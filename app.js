ed1cba3c8be916c49cc1653462cf5229363d7a60
TigerFishMan <shaaaik@live.com>
2021-11-13 07:58:03 +0500

    Initialized Squelize

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello world")
+const { Bot } = require("grammy");
+
+const bot = new Bot("643431269:AAHjExuktEe5vn9ubLH8W1ta1LgiL_EASnw");
+
+bot.command("start", (ctx) => ctx.reply(ctx));
+
+bot.start();
+
+console.log("Bot Started")
